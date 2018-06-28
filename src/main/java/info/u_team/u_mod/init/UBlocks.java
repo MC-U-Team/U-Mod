1520d5ff634914182a084f222b7e1f44899ea81d
HyCraftHD <hycrafthd@live.de>
2018-06-28 18:04:28 +0200

    Added some material ores (wip)

--- src/main/java/info/u_team/u_mod/init/UBlocks.java
+++ src/main/java/info/u_team/u_mod/init/UBlocks.java
@@ -4,13 +4,13 @@ import info.u_team.u_mod.UConstants;
 import info.u_team.u_mod.block.*;
 import info.u_team.u_team_core.block.*;
 import info.u_team.u_team_core.registry.BlockRegistry;
-import info.u_team.u_team_core.util.RegistryUtil;
+import info.u_team.u_team_core.util.*;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
 public class UBlocks {
 	
-	//public static final UBlock ore = new UBlockMetaData("ore", Material.ROCK, null);
+	public static final UBlock ore = new UBlockMetaData("ore", Material.ROCK, UCreativeTabs.MATERIAL, NonNullListUtil.createMeta("alluminum", "cadmium", "chromium", "cobalt", "copper", "lead", "molybdenum", "nickel", "platinum", "silver", "tantalum", "tin", "tungsten", "zinc"));
 	
 	public static final UBlock pulverizer = new UBlockPulveriser("pulverizer");
 	public static final UBlock battery = new UBlockBattery("battery");
--- src/main/java/info/u_team/u_mod/init/UCreativeTabs.java
+++ src/main/java/info/u_team/u_mod/init/UCreativeTabs.java
@@ -6,6 +6,7 @@ import info.u_team.u_team_core.creativetab.UCreativeTab;
 public class UCreativeTabs {
 	
 	public static final UCreativeTab MACHINE = new UCreativeTab(UConstants.MODID, "machine");
+	public static final UCreativeTab MATERIAL = new UCreativeTab(UConstants.MODID, "material");
 	
 	public static void init() {
 		MACHINE.setIcon(UBlocks.pulverizer); // TODO pulverizer is not displayed, why?? Maybe model error
