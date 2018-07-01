f5e0e3f670e19a8a4a7179f74da47f996f5dcb24
HyCraftHD <hycrafthd@live.de>
2018-07-01 18:51:57 +0200

    Added Alloys and CraftedResources enum (wip)

---
+++ src/main/java/info/u_team/u_mod/resources/EnumCraftedResources.java
@@ -0,0 +1,38 @@
+package info.u_team.u_mod.resources;
+
+import java.util.*;
+
+import info.u_team.u_team_core.api.IUMetaType;
+import info.u_team.u_team_core.util.NonNullListCustom;
+import net.minecraft.util.NonNullList;
+
+public enum EnumCraftedResources {
+	
+	COALCHUNK("coalchunk"),
+	CARBON("carbon");
+	
+	private String name;
+	
+	private EnumCraftedResources(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public static NonNullList<IUMetaType> createItemList() {
+		List<IUMetaType> list = new ArrayList<>();
+		for (EnumCraftedResources entry : values()) {
+			list.add(new IUMetaType() {
+				
+				@Override
+				public String getName() {
+					return entry.getName();
+				}
+			});
+		}
+		return new NonNullListCustom<>(list, null);
+	}
+	
+}
