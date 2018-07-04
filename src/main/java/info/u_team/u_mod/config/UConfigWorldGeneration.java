e7623b4b0988e163f10282c679306eebfdfc8442
HyCraftHD <hycrafthd@live.de>
2018-07-05 01:11:47 +0200

    Worldgeneration is now working

--- src/main/java/info/u_team/u_mod/config/UConfigWorldGeneration.java
+++ src/main/java/info/u_team/u_mod/config/UConfigWorldGeneration.java
@@ -12,57 +12,57 @@ public class UConfigWorldGeneration {
 	public static boolean enabled = true;
 	
 	@Comment("Ore generation aluminium")
-	public static GeneratableOre oreAluminium = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreAluminium = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation beryllium")
-	public static GeneratableOre oreBeryllium = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreBeryllium = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation cadmium")
-	public static GeneratableOre oreCadmium = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreCadmium = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation chromium")
-	public static GeneratableOre oreChromium = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreChromium = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation cobalt")
-	public static GeneratableOre oreCobalt = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreCobalt = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation copper")
-	public static GeneratableOre oreCopper = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreCopper = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation gallium")
-	public static GeneratableOre oreGallium = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreGallium = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation graphite")
-	public static GeneratableOre oreGraphite = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreGraphite = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation iridium")
-	public static GeneratableOre oreIridium = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreIridium = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation lead")
-	public static GeneratableOre oreLead = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreLead = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation magnesium")
-	public static GeneratableOre oreMagnesium = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreMagnesium = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation molybdenum")
-	public static GeneratableOre oreMolybdenum = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreMolybdenum = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation nickel")
-	public static GeneratableOre oreNickel = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreNickel = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation palladium")
-	public static GeneratableOre orePalladium = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre orePalladium = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation platinum")
-	public static GeneratableOre orePlatinum = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre orePlatinum = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation plutonium")
-	public static GeneratableOre orePlutonium = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre orePlutonium = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation potassium")
-	public static GeneratableOre orePotassium = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre orePotassium = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation silver")
-	public static GeneratableOre oreSilver = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreSilver = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation sodium")
-	public static GeneratableOre oreSodium = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreSodium = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation tantalum")
-	public static GeneratableOre oreTantalum = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreTantalum = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation tin")
-	public static GeneratableOre oreTin = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreTin = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation tungsten")
-	public static GeneratableOre oreTungsten = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreTungsten = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation uranium")
-	public static GeneratableOre oreUranium = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreUranium = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation vanadium")
-	public static GeneratableOre oreVanadium = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreVanadium = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation zinc")
-	public static GeneratableOre oreZinc = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreZinc = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	@Comment("Ore generation zirconium")
-	public static GeneratableOre oreZirconium = new GeneratableOre(6, 1, 2, GenerationType.MINMAX, 16, 64);
+	public static GeneratableOre oreZirconium = new GeneratableOre(6, 1, 8, GenerationType.MINMAX, 16, 64);
 	
 	public static class GeneratableOre {
 		
--- src/main/java/info/u_team/u_mod/world/generation/UWorldGeneration.java
+++ src/main/java/info/u_team/u_mod/world/generation/UWorldGeneration.java
@@ -2,16 +2,12 @@ package info.u_team.u_mod.world.generation;
 
 import java.util.Random;
 
-import com.google.common.base.Predicate;
-
 import info.u_team.u_mod.config.UConfigWorldGeneration;
 import info.u_team.u_mod.config.UConfigWorldGeneration.GeneratableOre;
 import info.u_team.u_mod.config.UConfigWorldGeneration.GeneratableOre.GenerationType;
 import info.u_team.u_mod.init.UBlocks;
-import info.u_team.u_mod.resource.EnumResources1;
-import net.minecraft.block.*;
+import info.u_team.u_mod.resource.*;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.*;
 import net.minecraft.world.chunk.IChunkProvider;
@@ -27,8 +23,37 @@ public class UWorldGeneration implements IWorldGenerator {
 		DimensionType dimension = world.provider.getDimensionType();
 		
 		if (dimension == DimensionType.OVERWORLD) {
+			
 			// Ores
+			// 1
 			generateOreFromConfig(UConfigWorldGeneration.oreAluminium, EnumResources1.ALUMINUM.getBlockState(UBlocks.resource_ore1), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreBeryllium, EnumResources1.BERYLLIUM.getBlockState(UBlocks.resource_ore1), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreCadmium, EnumResources1.CADMIUM.getBlockState(UBlocks.resource_ore1), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreChromium, EnumResources1.CHROMIUM.getBlockState(UBlocks.resource_ore1), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreCobalt, EnumResources1.COBALT.getBlockState(UBlocks.resource_ore1), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreCopper, EnumResources1.COPPER.getBlockState(UBlocks.resource_ore1), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreGallium, EnumResources1.GALLIUM.getBlockState(UBlocks.resource_ore1), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreGraphite, EnumResources1.GRAPHITE.getBlockState(UBlocks.resource_ore1), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreIridium, EnumResources1.IRIDIUM.getBlockState(UBlocks.resource_ore1), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreLead, EnumResources1.LEAD.getBlockState(UBlocks.resource_ore1), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreMagnesium, EnumResources1.MAGNESIUM.getBlockState(UBlocks.resource_ore1), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreMolybdenum, EnumResources1.MOLYBDENUM.getBlockState(UBlocks.resource_ore1), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreNickel, EnumResources1.NICKEL.getBlockState(UBlocks.resource_ore1), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.orePalladium, EnumResources1.PALLADIUM.getBlockState(UBlocks.resource_ore1), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.orePlatinum, EnumResources1.PLATINUM.getBlockState(UBlocks.resource_ore1), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.orePlutonium, EnumResources1.PLUTONIUM.getBlockState(UBlocks.resource_ore1), chunkpos, world, random);
+			// 2
+			generateOreFromConfig(UConfigWorldGeneration.orePotassium, EnumResources2.POTASSIUM.getBlockState(UBlocks.resource_ore2), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreSilver, EnumResources2.SILVER.getBlockState(UBlocks.resource_ore2), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreSodium, EnumResources2.SODIUM.getBlockState(UBlocks.resource_ore2), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreTantalum, EnumResources2.TANTALUM.getBlockState(UBlocks.resource_ore2), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreTin, EnumResources2.TIN.getBlockState(UBlocks.resource_ore2), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreTungsten, EnumResources2.TUNGSTEN.getBlockState(UBlocks.resource_ore2), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreUranium, EnumResources2.URANIUM.getBlockState(UBlocks.resource_ore2), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreVanadium, EnumResources2.VANADIUM.getBlockState(UBlocks.resource_ore2), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreZinc, EnumResources2.ZINC.getBlockState(UBlocks.resource_ore2), chunkpos, world, random);
+			generateOreFromConfig(UConfigWorldGeneration.oreZirconium, EnumResources2.ZIRCONIUM.getBlockState(UBlocks.resource_ore2), chunkpos, world, random);
+			
 		}
 	}
 	
@@ -41,14 +66,6 @@ public class UWorldGeneration implements IWorldGenerator {
 	}
 	
 	private void generateOreMinMax(IBlockState state, BlockPos pos, World world, Random random, int count, double chance, int veinsize, int min, int max) {
-		generateOreMinMax(state, pos, world, random, getStoneReplacable(), count, chance, veinsize, min, max);
-	}
-	
-	private void generateOreCenterSpread(IBlockState state, BlockPos pos, World world, Random random, int count, double chance, int veinsize, int center, int spread) {
-		generateOreCenterSpread(state, pos, world, random, getStoneReplacable(), count, chance, veinsize, center, spread);
-	}
-	
-	private void generateOreMinMax(IBlockState state, BlockPos pos, World world, Random random, Predicate<IBlockState> allowed, int count, double chance, int veinsize, int min, int max) {
 		if (max < min) {
 			int i = min;
 			min = max;
@@ -60,34 +77,23 @@ public class UWorldGeneration implements IWorldGenerator {
 				--min;
 			}
 		}
-		WorldGenMinable generator = new WorldGenMinable(state, veinsize, allowed);
+		WorldGenMinable generator = new WorldGenMinable(state, veinsize);
 		for (int i = 0; i < count; i++) {
 			if (random.nextDouble() <= chance) {
-				generator.generate(world, random, pos.add(random.nextInt(16), random.nextInt(max - min) + min, random.nextInt(16)));
+				BlockPos randpos = pos.add(random.nextInt(16), random.nextInt(max - min) + min, random.nextInt(16));
+				generator.generate(world, random, randpos);
 			}
 		}
 	}
 	
-	private void generateOreCenterSpread(IBlockState state, BlockPos pos, World world, Random random, Predicate<IBlockState> allowed, int count, double chance, int veinsize, int center, int spread) {
-		WorldGenMinable generator = new WorldGenMinable(state, veinsize, allowed);
+	private void generateOreCenterSpread(IBlockState state, BlockPos pos, World world, Random random, int count, double chance, int veinsize, int center, int spread) {
+		WorldGenMinable generator = new WorldGenMinable(state, veinsize);
 		for (int i = 0; i < count; i++) {
 			if (random.nextDouble() <= chance) {
-				generator.generate(world, random, pos.add(random.nextInt(16), random.nextInt(spread) + random.nextInt(spread) + center - spread, random.nextInt(16)));
+				BlockPos randpos = pos.add(random.nextInt(16), random.nextInt(spread) + random.nextInt(spread) + center - spread, random.nextInt(16));
+				generator.generate(world, random, randpos);
 			}
 		}
 	}
-	
-	private Predicate<IBlockState> getStoneReplacable() {
-		return (input) -> {
-			return (input != null && input.getBlock() == Blocks.STONE && input.getValue(BlockStone.VARIANT).isNatural());
-		};
-	}
-	
-	// Enable if needed
-	// private Predicate<IBlockState> getBlockReplacable(Block block) {
-	// return (input) -> {
-	// return input != null && input.getBlock() == block;
-	// };
-	// }
 	
 }
