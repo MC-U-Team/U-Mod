049582db0f9dcdd493d607b3cdb5a071487187c6
HyCraftHD <hycrafthd@live.de>
2018-07-06 02:04:07 +0200

    Some biome things

--- src/main/java/info/u_team/u_mod/world/biome/BiomeContamined.java
+++ src/main/java/info/u_team/u_mod/world/biome/BiomeContamined.java
@@ -1,11 +1,13 @@
 package info.u_team.u_mod.world.biome;
 
 import info.u_team.u_team_core.biome.UBiome;
+import net.minecraft.init.Blocks;
 
 public class BiomeContamined extends UBiome {
-
+	
 	public BiomeContamined(String name) {
 		super(name);
+		topBlock = Blocks.COBBLESTONE.getDefaultState();
 	}
 	
 }
