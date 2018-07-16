c6cfccc8f634698ce364129561dbf7a33a42a380
MrTroble <nifroh56@gmail.com>
2018-07-16 21:34:03 +0200

    Added battery model

--- src/main/java/info/u_team/u_mod/tilentity/TileEntityAlloyFurnace.java
+++ src/main/java/info/u_team/u_mod/tilentity/TileEntityAlloyFurnace.java
@@ -22,7 +22,7 @@ public class TileEntityAlloyFurnace extends TileEntityMachine {
 	private int recipe = -1;
 		
 	public TileEntityAlloyFurnace() {
-		super(2, "alloy_furnace");
+		super(4, "alloy_furnace");
 	}
 	
 	@Override
