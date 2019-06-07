c3dbabc4c4dc73ce34e1313432418825e3d200d4
HyCraftHD <hycrafthd@live.de>
2019-06-07 03:03:59 +0200

    Updated build properties

--- src/main/java/info/u_team/u_mod/block/BlockSolarPanel.java
+++ src/main/java/info/u_team/u_mod/block/BlockSolarPanel.java
@@ -3,7 +3,7 @@ package info.u_team.u_mod.block;
 import info.u_team.u_mod.api.machines.ISolarPanel;
 import info.u_team.u_mod.init.*;
 import info.u_team.u_team_core.block.UBlockTileEntity;
-import info.u_team.u_team_core.util.BlockProperties;
+import info.u_team.u_team_core.util.*;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
@@ -17,9 +17,8 @@ public class BlockSolarPanel extends UBlockTileEntity {
 	protected final ISolarPanel solarpanel;
 	
 	public BlockSolarPanel(String name, ISolarPanel solarpanel) {
-		super(name + "_" + solarpanel.getName(), UModItemGroups.machines, new BlockProperties(Material.IRON, Material.IRON.getColor()).hardnessAndResistance(1F).sound(SoundType.METAL), UModTileEntityTypes.solarpanel);
+		super(name + "_" + solarpanel.getName(), UModItemGroups.machines, new BlockProperties(Material.IRON, Material.IRON.getColor()).hardnessAndResistance(1F).sound(SoundType.METAL), new ItemProperties().rarity(solarpanel.getRarity()), UModTileEntityTypes.solarpanel);
 		this.solarpanel = solarpanel;
-		// TODO itemblock
 	}
 	
 	@Override
--- src/main/java/info/u_team/u_mod/proxy/CommonProxy.java
+++ src/main/java/info/u_team/u_mod/proxy/CommonProxy.java
@@ -2,11 +2,15 @@ package info.u_team.u_mod.proxy;
 
 import info.u_team.u_mod.init.*;
 import info.u_team.u_team_core.api.IModProxy;
+import info.u_team.u_team_core.intern.config.ClientConfig;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.config.ModConfig.Type;
 
 public class CommonProxy implements IModProxy {
 	
 	@Override
 	public void construct() {
+		ModLoadingContext.get().registerConfig(Type.CLIENT, ClientConfig.config);
 		UModBlocks.construct();
 		UModTileEntityTypes.construct();
 	}
