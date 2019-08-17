8fb71c4ae4f608b20d6fbc20e256c27d990aecac
HyCraftHD <hycrafthd@live.de>
2019-08-17 20:24:28 +0200

    Rename UModGroups to UModItemGroups

--- src/main/java/info/u_team/u_mod/init/UModItemGroups.java
+++ src/main/java/info/u_team/u_mod/init/UModItemGroups.java
@@ -4,7 +4,7 @@ import info.u_team.u_mod.UMod;
 import info.u_team.u_team_core.itemgroup.UItemGroup;
 import net.minecraft.item.ItemGroup;
 
-public class UModGroups {
+public class UModItemGroups {
 	
 	public static final ItemGroup GROUP = new UItemGroup(UMod.MODID, "goup", () -> UModBlocks.CRATE);
 	
