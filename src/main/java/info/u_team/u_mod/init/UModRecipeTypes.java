7c9d213884af9e8ee0c47edbbf4f389bce99bc73
HyCraftHD <hycrafthd@live.de>
2019-11-11 21:37:17 +0100

    Make the recipe more abstract

--- src/main/java/info/u_team/u_mod/init/UModRecipeTypes.java
+++ src/main/java/info/u_team/u_mod/init/UModRecipeTypes.java
@@ -1,8 +1,5 @@
 package info.u_team.u_mod.init;
 
-import info.u_team.u_mod.recipe.MachineRecipe;
-import net.minecraft.item.crafting.IRecipeType;
-
 public class UModRecipeTypes {
 	
 }
--- src/main/java/info/u_team/u_mod/recipe/MachineRecipe.java
+++ src/main/java/info/u_team/u_mod/recipe/MachineRecipe.java
@@ -1,66 +1,38 @@
 package info.u_team.u_mod.recipe;
 
-import java.util.*;
-
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.*;
 import net.minecraft.util.*;
 import net.minecraft.world.World;
-import net.minecraftforge.common.util.RecipeMatcher;
 
-public class MachineRecipe implements IRecipe<IInventory> {
+public abstract class MachineRecipe implements IRecipe<IInventory> {
 	
-	private final ResourceLocation location;
-	private final IRecipeType<?> type;
-	private final IRecipeSerializer<?> serializer;
-	private final NonNullList<Ingredient> ingredients;
-	private final NonNullList<ItemStack> outputs;
-	private final int time;
-	private final int consumptionOnStart;
-	private final int consumptionPerTick;
-	private final boolean isSimple;
+	protected final ResourceLocation location;
+	protected final IRecipeType<?> type;
+	protected final IRecipeSerializer<?> serializer;
+	protected final int totalTime;
+	protected final int consumptionOnStart;
+	protected final int consumptionPerTick;
 	
-	public MachineRecipe(ResourceLocation location, IRecipeType<?> type, IRecipeSerializer<?> serializer, NonNullList<Ingredient> ingredients, NonNullList<ItemStack> outputs, int time, int consumptionOnStart, int consumptionPerTick) {
+	public MachineRecipe(ResourceLocation location, IRecipeType<?> type, IRecipeSerializer<?> serializer, NonNullList<Ingredient> ingredients, NonNullList<ItemStack> outputs, int totalTime, int consumptionOnStart, int consumptionPerTick) {
 		this.location = location;
 		this.type = type;
 		this.serializer = serializer;
-		this.ingredients = ingredients;
-		this.outputs = outputs;
-		this.time = time;
+		this.totalTime = totalTime;
 		this.consumptionOnStart = consumptionOnStart;
 		this.consumptionPerTick = consumptionPerTick;
-		this.isSimple = ingredients.stream().allMatch(Ingredient::isSimple);
 	}
 	
 	@Override
-	public boolean matches(IInventory inventory, World world) {
-		final RecipeItemHelper helper = new RecipeItemHelper();
-		final List<ItemStack> inputs = new ArrayList<>();
-		int validInputCount = 0;
-		
-		for (int index = 0; index < inventory.getSizeInventory(); ++index) {
-			final ItemStack stack = inventory.getStackInSlot(index);
-			if (!stack.isEmpty()) {
-				++validInputCount;
-				if (isSimple) {
-					helper.func_221264_a(stack, 1);
-				} else {
-					inputs.add(stack);
-				}
-			}
-		}
-		return validInputCount == ingredients.size() && (isSimple ? helper.canCraft(this, null) : RecipeMatcher.findMatches(inputs, ingredients) != null);
-	}
+	public abstract boolean matches(IInventory inventory, World world);
 	
-	public NonNullList<ItemStack> getOutput(IInventory inventory) {
-		return outputs;
-	}
+	public abstract NonNullList<ItemStack> getOutput(IInventory inventory);
 	
 	// Getter
 	
-	public int getTime() {
-		return time;
+	public int getTotalTime() {
+		return totalTime;
 	}
 	
 	public int getConsumptionOnStart() {
@@ -94,11 +66,6 @@ public class MachineRecipe implements IRecipe<IInventory> {
 	@Override
 	public boolean canFit(int width, int height) {
 		return true;
-	}
-	
-	@Override
-	public NonNullList<Ingredient> getIngredients() {
-		return ingredients;
 	}
 	
 	// Unused
