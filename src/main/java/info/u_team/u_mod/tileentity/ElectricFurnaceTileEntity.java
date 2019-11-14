de606792ade1c58373040a8fd09bea1ad67d1138
HyCraftHD <hycrafthd@live.de>
2019-11-14 17:46:45 +0100

    Implement electric furnace tile entity with abstract basic machine tile entity (reduce code!)

--- src/main/java/info/u_team/u_mod/tileentity/ElectricFurnaceTileEntity.java
+++ src/main/java/info/u_team/u_mod/tileentity/ElectricFurnaceTileEntity.java
@@ -2,43 +2,17 @@ package info.u_team.u_mod.tileentity;
 
 import info.u_team.u_mod.container.ElectricFurnaceContainer;
 import info.u_team.u_mod.init.UModTileEntityTypes;
-import info.u_team.u_mod.tileentity.basic.BasicContainerEnergyTileEntity;
-import info.u_team.u_mod.util.recipe.*;
+import info.u_team.u_mod.tileentity.basic.BasicMachineTileEntity;
+import info.u_team.u_mod.util.recipe.RecipeData;
 import net.minecraft.entity.player.*;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.item.crafting.*;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.text.*;
-import net.minecraftforge.api.distmarker.*;
 
-public class ElectricFurnaceTileEntity extends BasicContainerEnergyTileEntity {
-	
-	private final RecipeHandler<FurnaceRecipe> recipeHandler;
+public class ElectricFurnaceTileEntity extends BasicMachineTileEntity<FurnaceRecipe> {
 	
 	public ElectricFurnaceTileEntity() {
-		super(UModTileEntityTypes.ENERGY_FURNANCE, 20000, 100, 0, 10000);
-		recipeHandler = new TileEntityRecipeHandler<>(this, IRecipeType.SMELTING, 1, 1, RecipeData.getBasicCooking(0, 5));
-	}
-	
-	// Tick
-	@Override
-	protected void tickServer() {
-		recipeHandler.update(world);
-	}
-	
-	// NBT
-	
-	@Override
-	public void writeNBT(CompoundNBT compound) {
-		super.writeNBT(compound);
-		compound.put("recipe_data", recipeHandler.serializeNBT());
-	}
-	
-	@Override
-	public void readNBT(CompoundNBT compound) {
-		super.readNBT(compound);
-		recipeHandler.deserializeNBT(compound.getCompound("recipe_data"));
+		super(UModTileEntityTypes.ENERGY_FURNANCE, 20000, 100, 0, IRecipeType.SMELTING, 1, 1, RecipeData.getBasicCooking(0, 5));
 	}
 	
 	// Container
@@ -53,30 +27,4 @@ public class ElectricFurnaceTileEntity extends BasicContainerEnergyTileEntity {
 		return new ElectricFurnaceContainer(id, playerInventory, this);
 	}
 	
-	// Invalidate lazy optional
-	
-	@Override
-	public void remove() {
-		super.remove();
-		recipeHandler.invalidate();
-	}
-	
-	// Inital send when container is opened
-	@Override
-	public void sendInitialDataBuffer(PacketBuffer buffer) {
-		super.sendInitialDataBuffer(buffer);
-		recipeHandler.sendInitialDataBuffer(buffer);
-	}
-	
-	@OnlyIn(Dist.CLIENT)
-	@Override
-	public void handleInitialDataBuffer(PacketBuffer buffer) {
-		super.handleInitialDataBuffer(buffer);
-		recipeHandler.handleInitialDataBuffer(buffer);
-	}
-	
-	// Getter
-	public RecipeHandler<FurnaceRecipe> getRecipeHandler() {
-		return recipeHandler;
-	}
 }
