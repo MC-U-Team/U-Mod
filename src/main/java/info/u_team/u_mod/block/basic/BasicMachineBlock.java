5deeacdd03b76f9075fd04f7ddffb539c6988a0c
HyCraftHD <hycrafthd@live.de>
2019-11-15 23:49:50 +0100

    Drop block with tile entity data

--- src/main/java/info/u_team/u_mod/block/basic/BasicMachineBlock.java
+++ src/main/java/info/u_team/u_mod/block/basic/BasicMachineBlock.java
@@ -3,15 +3,17 @@ package info.u_team.u_mod.block.basic;
 import java.util.function.Supplier;
 
 import info.u_team.u_mod.init.UModItemGroups;
+import info.u_team.u_mod.tileentity.basic.BasicMachineTileEntity;
 import info.u_team.u_team_core.block.UTileEntityBlock;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.*;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer.Builder;
 import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.tileentity.*;
 import net.minecraft.util.*;
 import net.minecraft.util.math.*;
 import net.minecraft.world.World;
@@ -28,6 +30,20 @@ public class BasicMachineBlock extends UTileEntityBlock {
 	@Override
 	public boolean onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult hit) {
 		return openContainer(world, pos, player, true);
+	}
+	
+	// Drop
+	
+	@Override
+	public void onReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean isMoving) {
+		if (state.getBlock() != newState.getBlock()) {
+			isTileEntityFromType(world, pos).map(BasicMachineTileEntity.class::cast).ifPresent(tileEntity -> {
+				final ItemStack stack = new ItemStack(this);
+				tileEntity.writeNBT(stack.getOrCreateChildTag("BlockEntityTag"));
+				spawnAsEntity(world, pos, stack);
+			});
+			world.removeTileEntity(pos);
+		}
 	}
 	
 	// Facing
