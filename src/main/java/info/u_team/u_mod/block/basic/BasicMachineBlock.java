88c919602642d30670b3776ab342cf07b0bfed91
HyCraftHD <hycrafthd@live.de>
2019-11-15 13:56:28 +0100

    Add basic machine block with facing properties

---
+++ src/main/java/info/u_team/u_mod/block/basic/BasicMachineBlock.java
@@ -0,0 +1,52 @@
+package info.u_team.u_mod.block.basic;
+
+import info.u_team.u_mod.init.*;
+import info.u_team.u_team_core.block.UTileEntityBlock;
+import net.minecraft.block.*;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer.Builder;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.*;
+import net.minecraft.util.math.*;
+import net.minecraft.world.World;
+
+public class BasicMachineBlock extends UTileEntityBlock {
+	
+	public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+	
+	public BasicMachineBlock(String name) {
+		super(name, UModItemGroups.GROUP, Properties.create(Material.IRON), () -> UModTileEntityTypes.ENERGY_FURNANCE);
+		setDefaultState(getDefaultState().with(FACING, Direction.NORTH));
+	}
+	
+	@Override
+	public boolean onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult hit) {
+		return openContainer(world, pos, player, true);
+	}
+	
+	// Facing
+	
+	@Override
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+		return getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+	}
+	
+	@Override
+	protected void fillStateContainer(Builder<Block, BlockState> builder) {
+		builder.add(FACING);
+	}
+	
+	@Override
+	public BlockState rotate(BlockState state, Rotation rotation) {
+		return state.with(FACING, rotation.rotate(state.get(FACING)));
+	}
+	
+	@Override
+	public BlockState mirror(BlockState state, Mirror mirror) {
+		return state.rotate(mirror.toRotation(state.get(FACING)));
+	}
+	
+}
