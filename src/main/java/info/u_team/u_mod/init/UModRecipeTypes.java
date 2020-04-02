f4812d0cfb62f483d6d5f573a2adb61a81f79093
HyCraftHD <hycrafthd@live.de>
2020-04-02 20:25:25 +0200

    Register crusher recipe type

--- src/main/java/info/u_team/u_mod/init/UModRecipeTypes.java
+++ src/main/java/info/u_team/u_mod/init/UModRecipeTypes.java
@@ -1,5 +1,15 @@
 package info.u_team.u_mod.init;
 
+import info.u_team.u_mod.UMod;
+import info.u_team.u_mod.recipe.MachineRecipe;
+import net.minecraft.item.crafting.*;
+
 public class UModRecipeTypes {
+	
+	public static final IRecipeType<MachineRecipe> CRUSHER = register("crusher");
+	
+	private static <T extends IRecipe<?>> IRecipeType<T> register(String name) {
+		return IRecipeType.register(UMod.MODID + ":" + name);
+	}
 	
 }
