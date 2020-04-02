5c875277e9ca6711d152d671f7bea519a45da34d
HyCraftHD <hycrafthd@live.de>
2020-04-02 14:26:51 +0200

    Remove old things that are now in uteamcore

--- src/main/java/info/u_team/u_mod/data/provider/UModLootTablesProvider.java
+++ src/main/java/info/u_team/u_mod/data/provider/UModLootTablesProvider.java
@@ -3,11 +3,9 @@ package info.u_team.u_mod.data.provider;
 import java.util.function.BiConsumer;
 
 import info.u_team.u_mod.init.UModBlocks;
-import info.u_team.u_mod.loot.SetTileEntityNBT;
 import info.u_team.u_team_core.data.*;
-import net.minecraft.util.*;
-import net.minecraft.world.storage.loot.*;
-import net.minecraft.world.storage.loot.conditions.SurvivesExplosion;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.LootTable;
 
 public class UModLootTablesProvider extends CommonLootTablesProvider {
 	
@@ -17,18 +15,6 @@ public class UModLootTablesProvider extends CommonLootTablesProvider {
 	
 	@Override
 	protected void registerLootTables(BiConsumer<ResourceLocation, LootTable> consumer) {
-		registerBlock(UModBlocks.ELECTRIC_FURNACE, addTileEntityLootTable(UModBlocks.ELECTRIC_FURNACE), consumer);
+		registerBlock(UModBlocks.ELECTRIC_FURNACE, addTileEntityBlockLootTable(UModBlocks.ELECTRIC_FURNACE), consumer);
 	}
-	
-	protected static LootTable addTileEntityLootTable(IItemProvider item) {
-		return LootTable.builder() //
-				.setParameterSet(LootParameterSets.BLOCK) //
-				.addLootPool(LootPool.builder() //
-						.rolls(ConstantRange.of(1)) //
-						.addEntry(ItemLootEntry.builder(item)) //
-						.acceptFunction(SetTileEntityNBT.builder()) //
-						.acceptCondition(SurvivesExplosion.builder())) //
-				.build();
-	}
-	
 }
