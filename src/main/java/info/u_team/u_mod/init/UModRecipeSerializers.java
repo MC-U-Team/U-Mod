032d65cf9216e973bd4c31cb0e5a8202dc0b29e7
HyCraftHD <hycrafthd@live.de>
2020-04-02 21:28:25 +0200

    Register serializer

--- src/main/java/info/u_team/u_mod/init/UModRecipeSerializers.java
+++ src/main/java/info/u_team/u_mod/init/UModRecipeSerializers.java
@@ -1,6 +1,22 @@
 package info.u_team.u_mod.init;
 
+import info.u_team.u_mod.UMod;
+import info.u_team.u_mod.recipe.OneIngredientMachineRecipe;
+import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
+@EventBusSubscriber(bus = Bus.MOD, modid = UMod.MODID)
 public class UModRecipeSerializers {
+	
+	public static final OneIngredientMachineRecipe.Serializer<OneIngredientMachineRecipe> CRUSHER = new OneIngredientMachineRecipe.Serializer<>("crusher", UModRecipeTypes.CRUSHER);
+	
+	@SubscribeEvent
+	public static void register(Register<IRecipeSerializer<?>> event) {
+		BaseRegistryUtil.getAllGenericRegistryEntriesAndApplyNames(UMod.MODID, IRecipeSerializer.class).forEach(event.getRegistry()::register);
+	}
 	
 }
