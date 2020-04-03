ffe973ddaed15e9d06e48cc8af3a99a361cc9985
HyCraftHD <hycrafthd@live.de>
2020-04-03 15:13:47 +0200

    Add recipe types

--- src/integration/java/info/u_team/u_mod/integration/jei/UModJeiPlugin.java
+++ src/integration/java/info/u_team/u_mod/integration/jei/UModJeiPlugin.java
@@ -1,13 +1,18 @@
 package info.u_team.u_mod.integration.jei;
 
+import java.util.*;
+
 import info.u_team.u_mod.UMod;
-import info.u_team.u_mod.init.UModBlocks;
+import info.u_team.u_mod.init.*;
 import info.u_team.u_mod.integration.jei.category.CrusherRecipeCategoryJei;
 import mezz.jei.api.*;
 import mezz.jei.api.constants.VanillaRecipeCategoryUid;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.registration.*;
+import net.minecraft.client.Minecraft;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.*;
 import net.minecraft.util.ResourceLocation;
 
 @JeiPlugin
@@ -24,6 +29,15 @@ public class UModJeiPlugin implements IModPlugin {
 	public void registerCategories(IRecipeCategoryRegistration registration) {
 		final IGuiHelper helper = registration.getJeiHelpers().getGuiHelper();
 		registration.addRecipeCategories(new CrusherRecipeCategoryJei(helper));
+	}
+	
+	@Override
+	public void registerRecipes(IRecipeRegistration registration) {
+		registration.addRecipes(getRecipes(UModRecipeTypes.CRUSHER), CrusherRecipeCategoryJei.ID);
+	}
+	
+	private static <C extends IInventory, T extends IRecipe<C>> Collection<IRecipe<C>> getRecipes(IRecipeType<T> type) {
+		return Minecraft.getInstance().world.getRecipeManager().getRecipes(type).values();
 	}
 	
 	@Override
