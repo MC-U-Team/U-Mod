0cdc763c56148bea6da5c47ca39c59f7824aba11
HyCraftHD <hycrafthd@live.de>
2020-04-03 02:03:36 +0200

    Add crusher tile entity

---
+++ src/main/java/info/u_team/u_mod/tileentity/CrusherTileEntity.java
@@ -0,0 +1,30 @@
+package info.u_team.u_mod.tileentity;
+
+import info.u_team.u_mod.container.CrusherContainer;
+import info.u_team.u_mod.init.*;
+import info.u_team.u_mod.recipe.OneIngredientMachineRecipe;
+import info.u_team.u_mod.tileentity.basic.BasicMachineTileEntity;
+import info.u_team.u_mod.util.recipe.RecipeData;
+import net.minecraft.entity.player.*;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.util.text.*;
+
+public class CrusherTileEntity extends BasicMachineTileEntity<OneIngredientMachineRecipe> {
+	
+	public CrusherTileEntity() {
+		super(UModTileEntityTypes.CRUSHER, 20000, 100, 0, UModRecipeTypes.CRUSHER, 1, 1, RecipeData.getBasicMachine());
+	}
+	
+	// Container
+	
+	@Override
+	public ITextComponent getDisplayName() {
+		return new TranslationTextComponent("container.umod.electric_furnace");
+	}
+	
+	@Override
+	public Container createMenu(int id, PlayerInventory playerInventory, PlayerEntity player) {
+		return new CrusherContainer(id, playerInventory, this);
+	}
+	
+}
