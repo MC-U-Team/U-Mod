41719edc1772f3c0deaef7feadd547c5131ae5c1
HyCraftHD <hycrafthd@live.de>
2020-04-03 14:56:15 +0200

    Register category

--- src/integration/java/info/u_team/u_mod/integration/jei/UModJeiPlugin.java
+++ src/integration/java/info/u_team/u_mod/integration/jei/UModJeiPlugin.java
@@ -2,9 +2,11 @@ package info.u_team.u_mod.integration.jei;
 
 import info.u_team.u_mod.UMod;
 import info.u_team.u_mod.init.UModBlocks;
+import info.u_team.u_mod.integration.jei.category.CrusherRecipeCategoryJei;
 import mezz.jei.api.*;
 import mezz.jei.api.constants.VanillaRecipeCategoryUid;
-import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.registration.*;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
@@ -16,6 +18,12 @@ public class UModJeiPlugin implements IModPlugin {
 	@Override
 	public ResourceLocation getPluginUid() {
 		return id;
+	}
+	
+	@Override
+	public void registerCategories(IRecipeCategoryRegistration registration) {
+		final IGuiHelper helper = registration.getJeiHelpers().getGuiHelper();
+		registration.addRecipeCategories(new CrusherRecipeCategoryJei(helper));
 	}
 	
 	@Override
