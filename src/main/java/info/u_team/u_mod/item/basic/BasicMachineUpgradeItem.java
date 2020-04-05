d4198c4adca68d20da6a7ee804c60b789900aa84
HyCraftHD <hycrafthd@live.de>
2020-04-05 14:47:21 +0200

    Add basic upgrade

--- src/main/java/info/u_team/u_mod/item/basic/BasicMachineUpgradeItem.java
+++ src/main/java/info/u_team/u_mod/item/basic/BasicMachineUpgradeItem.java
@@ -4,13 +4,14 @@ import java.util.function.Function;
 
 import info.u_team.u_mod.init.UModItemGroups;
 import info.u_team.u_team_core.item.UItem;
+import net.minecraft.item.Rarity;
 
 public abstract class BasicMachineUpgradeItem extends UItem {
 	
 	protected final int maxStackSize;
 	
-	public BasicMachineUpgradeItem(String name, int maxStackSize) {
-		super(name, UModItemGroups.GROUP, new Properties().maxStackSize(maxStackSize));
+	public BasicMachineUpgradeItem(String name, int maxStackSize, Rarity rarity) {
+		super(name, UModItemGroups.GROUP, new Properties().maxStackSize(maxStackSize).rarity(rarity));
 		this.maxStackSize = maxStackSize;
 	}
 	
@@ -19,5 +20,9 @@ public abstract class BasicMachineUpgradeItem extends UItem {
 	public abstract Function<Integer, Integer> applyStartConsumptionUpgrade(int count);
 	
 	public abstract Function<Integer, Integer> applyTickConsumptionUpgrade(int count);
+	
+	protected int validateStackSize(int count) {
+		return Math.min(maxStackSize, count);
+	}
 	
 }
