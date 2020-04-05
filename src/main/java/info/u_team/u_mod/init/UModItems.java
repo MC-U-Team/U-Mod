5a02fec602a345282119056919cbedc798b12dcf
HyCraftHD <hycrafthd@live.de>
2020-04-05 14:47:34 +0200

    Register time upgrades

--- src/main/java/info/u_team/u_mod/init/UModItems.java
+++ src/main/java/info/u_team/u_mod/init/UModItems.java
@@ -1,7 +1,9 @@
 package info.u_team.u_mod.init;
 
 import info.u_team.u_mod.UMod;
-import net.minecraft.item.Item;
+import info.u_team.u_mod.item.TimeMachineUpgradeItem;
+import info.u_team.u_team_core.util.registry.BaseRegistryUtil;
+import net.minecraft.item.*;
 import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -10,7 +12,12 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 @EventBusSubscriber(bus = Bus.MOD, modid = UMod.MODID)
 public class UModItems {
 	
+	public static final TimeMachineUpgradeItem TIME_TIER_1_MACHINE_UPGRADE = new TimeMachineUpgradeItem("time_tier_1_machine_upgrade", 4, Rarity.UNCOMMON);
+	public static final TimeMachineUpgradeItem TIME_TIER_2_MACHINE_UPGRADE = new TimeMachineUpgradeItem("time_tier_2_machine_upgrade", 16, Rarity.RARE);
+	public static final TimeMachineUpgradeItem TIME_TIER_3_MACHINE_UPGRADE = new TimeMachineUpgradeItem("time_tier_3_machine_upgrade", 64, Rarity.EPIC);
+	
 	@SubscribeEvent
 	public static void register(Register<Item> event) {
+		BaseRegistryUtil.getAllRegistryEntriesAndApplyNames(UMod.MODID, Item.class).forEach(event.getRegistry()::register);
 	}
 }
