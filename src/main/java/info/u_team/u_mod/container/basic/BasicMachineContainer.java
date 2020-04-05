165cace725db50c4fd1087d63a6551b6442437ee
HyCraftHD <hycrafthd@live.de>
2020-04-05 13:45:02 +0200

    Now 7 is the std

--- src/main/java/info/u_team/u_mod/container/basic/BasicMachineContainer.java
+++ src/main/java/info/u_team/u_mod/container/basic/BasicMachineContainer.java
@@ -11,7 +11,7 @@ import net.minecraftforge.items.IItemHandler;
 
 public class BasicMachineContainer<T extends BasicMachineTileEntity<?>> extends UTileEntityContainer<T> {
 	
-	protected int playerInventoryStart = 2; // set standard to 2
+	protected int playerInventoryStart = 7; // set standard to 7
 	
 	// Client
 	public BasicMachineContainer(ContainerType<?> type, int id, PlayerInventory playerInventory, PacketBuffer buffer) {
