900c18551a7cf7eb70717deb7d44fd41a27a7df4
HyCraftHD <hycrafthd@live.de>
2020-04-05 23:49:11 +0200

    Add more utility methods to the observable tile entity stack handler

--- src/main/java/info/u_team/u_mod/util/inventory/ObservableTileEntityStackHandler.java
+++ src/main/java/info/u_team/u_mod/util/inventory/ObservableTileEntityStackHandler.java
@@ -1,5 +1,7 @@
 package info.u_team.u_mod.util.inventory;
 
+import java.util.*;
+import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
 import info.u_team.u_team_core.inventory.TileEntityUItemStackHandler;
@@ -7,18 +9,29 @@ import net.minecraft.tileentity.TileEntity;
 
 public class ObservableTileEntityStackHandler extends TileEntityUItemStackHandler {
 	
+	private final List<Consumer<Integer>> contentChangedObserverList;
+	
 	public ObservableTileEntityStackHandler(int size, TileEntity tileEntity) {
 		super(size, tileEntity);
+		contentChangedObserverList = new ArrayList<>();
 	}
 	
 	@Override
 	protected void onContentsChanged(int slot) {
 		super.onContentsChanged(slot);
-		
+		contentChangedObserverList.forEach(entry -> entry.accept(slot));
 	}
 	
 	public boolean isEmpty() {
 		return IntStream.range(0, getSlots()).allMatch(index -> getStackInSlot(index).isEmpty());
+	}
+	
+	public void addObserver(Consumer<Integer> consumer) {
+		contentChangedObserverList.add(consumer);
+	}
+	
+	public void removeObserver(Consumer<Integer> consumer) {
+		contentChangedObserverList.remove(consumer);
 	}
 	
 }
