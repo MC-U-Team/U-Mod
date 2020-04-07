e9d4e64ad44257af86ff04944b72f662ba1b2860
HyCraftHD <hycrafthd@live.de>
2020-04-07 17:08:10 +0200

    Update observable item stack handler

--- src/main/java/info/u_team/u_mod/util/inventory/ObservableStackHandler.java
+++ src/main/java/info/u_team/u_mod/util/inventory/ObservableStackHandler.java
@@ -5,31 +5,64 @@ import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
 import info.u_team.u_team_core.inventory.UItemStackHandler;
+import net.minecraft.item.ItemStack;
 
 public class ObservableStackHandler extends UItemStackHandler {
 	
-	private final List<Consumer<Integer>> contentChangedObserverList;
+	private final List<Consumer<Integer>> itemChangedObserverList;
 	
 	public ObservableStackHandler(int size) {
 		super(size);
-		contentChangedObserverList = new ArrayList<>();
+		itemChangedObserverList = new ArrayList<>();
 	}
 	
 	@Override
-	protected void onContentsChanged(int slot) {
-		contentChangedObserverList.forEach(entry -> entry.accept(slot));
+	public void setStackInSlot(int slot, ItemStack stack) {
+		final ItemStack previousStack = getStackInSlot(slot);
+		super.setStackInSlot(slot, stack);
+		if (hasStackChanged(previousStack, stack)) {
+			itemChanged(slot);
+		}
+	}
+	
+	@Override
+	public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
+		final ItemStack previousStack = getStackInSlot(slot);
+		final ItemStack toReturn = insertItem(slot, stack, simulate);
+		if (hasStackChanged(previousStack, getStackInSlot(slot))) {
+			itemChanged(slot);
+		}
+		return toReturn;
+	}
+	
+	@Override
+	public ItemStack extractItem(int slot, int amount, boolean simulate) {
+		final ItemStack previousStack = getStackInSlot(slot);
+		final ItemStack toReturn = extractItem(slot, amount, simulate);
+		if (hasStackChanged(previousStack, getStackInSlot(slot))) {
+			itemChanged(slot);
+		}
+		return toReturn;
 	}
 	
 	public boolean isEmpty() {
 		return IntStream.range(0, getSlots()).allMatch(index -> getStackInSlot(index).isEmpty());
 	}
 	
+	private boolean hasStackChanged(ItemStack a, ItemStack b) {
+		return !ItemStack.areItemStacksEqual(a, b);
+	}
+	
+	protected void itemChanged(int slot) {
+		itemChangedObserverList.forEach(entry -> entry.accept(slot));
+	}
+	
 	public void addObserver(Consumer<Integer> consumer) {
-		contentChangedObserverList.add(consumer);
+		itemChangedObserverList.add(consumer);
 	}
 	
 	public void removeObserver(Consumer<Integer> consumer) {
-		contentChangedObserverList.remove(consumer);
+		itemChangedObserverList.remove(consumer);
 	}
 	
 }
