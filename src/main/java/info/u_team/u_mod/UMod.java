4a5c8d362f52f157aaaad53873a8154b2601fc85
HyCraftHD <hycrafthd@live.de>
2020-04-21 14:30:53 +0200

    Add sign checker

--- src/main/java/info/u_team/u_mod/UMod.java
+++ src/main/java/info/u_team/u_mod/UMod.java
@@ -1,5 +1,6 @@
 package info.u_team.u_mod;
 
+import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(UMod.MODID)
@@ -8,7 +9,7 @@ public class UMod {
 	public static final String MODID = "umod";
 	
 	public UMod() {
-		
+		JarSignVerifier.checkSigned(MODID);
 	}
 	
 }
