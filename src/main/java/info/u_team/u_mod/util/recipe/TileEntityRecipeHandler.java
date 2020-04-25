ba901d417d4f0f0f32518f66875ac5e88edbf2a1
HyCraftHD <hycrafthd@live.de>
2020-04-25 13:52:27 +0200

    Use the default handler now again

--- src/main/java/info/u_team/u_mod/util/recipe/TileEntityRecipeHandler.java
+++ src/main/java/info/u_team/u_mod/util/recipe/TileEntityRecipeHandler.java
@@ -1,14 +1,14 @@
 package info.u_team.u_mod.util.recipe;
 
 import info.u_team.u_mod.tileentity.basic.BasicEnergyTileEntity;
-import info.u_team.u_mod.util.inventory.ObservableTileEntityStackHandler;
+import info.u_team.u_team_core.inventory.TileEntityUItemStackHandler;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.crafting.*;
 
 public class TileEntityRecipeHandler<T extends IRecipe<IInventory>, X extends BasicEnergyTileEntity> extends RecipeHandler<T> {
 	
 	public TileEntityRecipeHandler(X tileEntity, IRecipeType<T> recipeType, int ingredientSize, int outputSize, int upgradeSize, RecipeData<T> recipeData) {
-		super(recipeType, tileEntity.getInternalEnergyStorage(), new ObservableTileEntityStackHandler(ingredientSize, tileEntity), new ObservableTileEntityStackHandler(outputSize, tileEntity), new ObservableTileEntityStackHandler(upgradeSize, tileEntity), recipeData, tileEntity::markDirty);
+		super(recipeType, tileEntity.getInternalEnergyStorage(), new TileEntityUItemStackHandler(ingredientSize, tileEntity), new TileEntityUItemStackHandler(outputSize, tileEntity), new TileEntityUItemStackHandler(upgradeSize, tileEntity), recipeData, tileEntity::markDirty);
 	}
 	
 }
