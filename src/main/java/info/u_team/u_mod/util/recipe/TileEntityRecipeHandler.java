e6edf57fa7b1c1d3736ec4e9e9701e939d65c392
HyCraftHD <hycrafthd@live.de>
2020-05-01 00:35:43 +0200

    Fix the tile entity recipe handler

--- src/main/java/info/u_team/u_mod/util/recipe/TileEntityRecipeHandler.java
+++ src/main/java/info/u_team/u_mod/util/recipe/TileEntityRecipeHandler.java
@@ -1,14 +1,15 @@
 package info.u_team.u_mod.util.recipe;
 
 import info.u_team.u_mod.tileentity.basic.BasicEnergyTileEntity;
-import info.u_team.u_team_core.inventory.TileEntityUItemStackHandler;
+import info.u_team.u_team_core.inventory.UItemStackHandler;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.crafting.*;
+import net.minecraftforge.common.util.LazyOptional;
 
 public class TileEntityRecipeHandler<T extends IRecipe<IInventory>, X extends BasicEnergyTileEntity> extends RecipeHandler<T> {
 	
-	public TileEntityRecipeHandler(X tileEntity, IRecipeType<T> recipeType, int ingredientSize, int outputSize, int upgradeSize, RecipeData<T> recipeData) {
-		super(recipeType, tileEntity.getInternalEnergyStorage(), new TileEntityUItemStackHandler(ingredientSize, tileEntity), new TileEntityUItemStackHandler(outputSize, tileEntity), new TileEntityUItemStackHandler(upgradeSize, tileEntity), recipeData, tileEntity::markDirty);
+	public TileEntityRecipeHandler(X tileEntity, IRecipeType<T> recipeType, int ingredientSize, LazyOptional<UItemStackHandler> ingredientSlotsOptional, LazyOptional<UItemStackHandler> outputSlotsOptional, LazyOptional<UItemStackHandler> upgradeSlotsOptional, RecipeData<T> recipeData) {
+		super(recipeType, tileEntity.getInternalEnergyStorageOptional(), ingredientSize, ingredientSlotsOptional, outputSlotsOptional, upgradeSlotsOptional, recipeData, tileEntity::markDirty);
 	}
 	
 }
