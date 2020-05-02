15811ab91e53a3055ac5ee1a0f30d9ef30ef9655
HyCraftHD <hycrafthd@live.de>
2020-05-02 15:02:41 +0200

    Update the tile entity recipe handler as well

--- src/main/java/info/u_team/u_mod/util/recipe/TileEntityRecipeHandler.java
+++ src/main/java/info/u_team/u_mod/util/recipe/TileEntityRecipeHandler.java
@@ -1,5 +1,6 @@
 package info.u_team.u_mod.util.recipe;
 
+import info.u_team.u_mod.block.basic.BasicMachineBlock;
 import info.u_team.u_mod.tileentity.basic.BasicEnergyTileEntity;
 import info.u_team.u_team_core.inventory.UItemStackHandler;
 import net.minecraft.inventory.IInventory;
@@ -9,7 +10,12 @@ import net.minecraftforge.common.util.LazyOptional;
 public class TileEntityRecipeHandler<T extends IRecipe<IInventory>, X extends BasicEnergyTileEntity> extends RecipeHandler<T> {
 	
 	public TileEntityRecipeHandler(X tileEntity, IRecipeType<T> recipeType, int ingredientSize, LazyOptional<UItemStackHandler> ingredientSlotsOptional, LazyOptional<UItemStackHandler> outputSlotsOptional, LazyOptional<UItemStackHandler> upgradeSlotsOptional, RecipeData<T> recipeData) {
-		super(recipeType, tileEntity.getInternalEnergyStorageOptional(), ingredientSize, ingredientSlotsOptional, outputSlotsOptional, upgradeSlotsOptional, recipeData, tileEntity::markDirty);
+		super(recipeType, tileEntity.getInternalEnergyStorageOptional(), ingredientSize, ingredientSlotsOptional, outputSlotsOptional, upgradeSlotsOptional, recipeData, tileEntity::markDirty, newState -> {
+			if (newState != tileEntity.getBlockState().get(BasicMachineBlock.WORKING)) {
+				tileEntity.getWorld().setBlockState(tileEntity.getPos(), tileEntity.getBlockState(), 3);
+				tileEntity.updateContainingBlockInfo();
+			}
+		});
 	}
 	
 }
