e59ccb8da902800553d204366f9b43fc965c12ed
HyCraftHD <hycrafthd@live.de>
2020-05-05 11:15:42 +0200

    Add fluid inventory interface

---
+++ src/main/java/info/u_team/u_mod/api/IFluidInventory.java
@@ -0,0 +1,7 @@
+package info.u_team.u_mod.api;
+
+import net.minecraft.inventory.IInventory;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+
+public interface IFluidInventory extends IInventory, IFluidHandler {
+}
