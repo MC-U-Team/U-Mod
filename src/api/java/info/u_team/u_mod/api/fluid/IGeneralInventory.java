687b79f968b4f521632e45c697739872cc99bf8c
HyCraftHD <hycrafthd@live.de>
2020-05-06 14:38:46 +0200

    Rename to IGeneralInventory

--- src/api/java/info/u_team/u_mod/api/fluid/IGeneralInventory.java
+++ src/api/java/info/u_team/u_mod/api/fluid/IGeneralInventory.java
@@ -3,5 +3,6 @@ package info.u_team.u_mod.api.fluid;
 import net.minecraft.inventory.IInventory;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 
-public interface IFluidInventory extends IInventory, IFluidHandler {
+public interface IGeneralInventory extends IInventory, IFluidHandler {
+	
 }
