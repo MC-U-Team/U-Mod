718762237ba3d7c7e69935bc1a1a87b8c230a5d0
HyCraftHD <hycrafthd@live.de>
2020-05-14 18:07:42 +0200

    Use proper brackets

--- src/api/java/info/u_team/u_mod/api/fluid/FluidHandlerHelper.java
+++ src/api/java/info/u_team/u_mod/api/fluid/FluidHandlerHelper.java
@@ -5,15 +5,17 @@ import net.minecraftforge.fluids.FluidStack;
 public class FluidHandlerHelper {
 	
 	public static boolean canFluidStacksStack(FluidStack a, FluidStack b) {
-		if (a.isEmpty() || !(a.getFluid() == b.getFluid()) || a.hasTag() != b.hasTag())
+		if (a.isEmpty() || !(a.getFluid() == b.getFluid()) || a.hasTag() != b.hasTag()) {
 			return false;
+		}
 		
 		return (!a.hasTag() || a.getTag().equals(b.getTag()));
 	}
 	
 	public static FluidStack copyStackWithSize(FluidStack stack, int size) {
-		if (size == 0)
+		if (size == 0) {
 			return FluidStack.EMPTY;
+		}
 		final FluidStack copy = stack.copy();
 		copy.setAmount(size);
 		return copy;
