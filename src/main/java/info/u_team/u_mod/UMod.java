631bc9f008b142a2cd3497e1e3387a09daff703c
HyCraftHD <hycrafthd@live.de>
2020-05-16 17:40:40 +0200

    Add testnetwork

--- src/main/java/info/u_team/u_mod/UMod.java
+++ src/main/java/info/u_team/u_mod/UMod.java
@@ -1,5 +1,6 @@
 package info.u_team.u_mod;
 
+import info.u_team.to_uteam_core.TestNetwork;
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import net.minecraftforge.fml.common.Mod;
 
@@ -10,5 +11,6 @@ public class UMod {
 	
 	public UMod() {
 		JarSignVerifier.checkSigned(MODID);
+		TestNetwork.construct();
 	}
 }
