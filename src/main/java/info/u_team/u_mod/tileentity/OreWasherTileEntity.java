a922f17754b695306d1cfd062fc4c35daa9279fa
HyCraftHD <hycrafthd@live.de>
2020-05-17 15:11:42 +0200

    Add basic fluid slot

--- src/main/java/info/u_team/u_mod/tileentity/OreWasherTileEntity.java
+++ src/main/java/info/u_team/u_mod/tileentity/OreWasherTileEntity.java
@@ -4,15 +4,25 @@ import info.u_team.u_mod.container.OreWasherContainer;
 import info.u_team.u_mod.init.*;
 import info.u_team.u_mod.recipe.OneIngredientMachineRecipe;
 import info.u_team.u_mod.tileentity.basic.BasicMachineTileEntity;
+import info.u_team.u_mod.util.UFluidStackHandler;
 import info.u_team.u_mod.util.recipe.RecipeData;
 import net.minecraft.entity.player.*;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.util.text.*;
+import net.minecraftforge.common.util.LazyOptional;
 
 public class OreWasherTileEntity extends BasicMachineTileEntity<OneIngredientMachineRecipe> {
 	
+	protected final UFluidStackHandler fluidIngredientSlots;
+	
+	protected final LazyOptional<UFluidStackHandler> fluidIngredientSlotsOptional;
+	
 	public OreWasherTileEntity() {
 		super(UModTileEntityTypes.ORE_WASHER, 20000, 100, 0, UModRecipeTypes.CRUSHER, 1, 6, 3, RecipeData.getBasicMachine());
+		
+		fluidIngredientSlots = new UFluidStackHandler(1);
+		
+		fluidIngredientSlotsOptional = LazyOptional.of(() -> fluidIngredientSlots);
 	}
 	
 	// Container
@@ -27,4 +37,8 @@ public class OreWasherTileEntity extends BasicMachineTileEntity<OneIngredientMac
 		return new OreWasherContainer(id, playerInventory, this);
 	}
 	
+	// TODO test
+	public UFluidStackHandler getFluidIngredientSlots() {
+		return fluidIngredientSlots;
+	}
 }
