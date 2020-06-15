975b12f2538edb65d9720f0bf870c1aa32e41b97
HyCraftHD <hycrafthd@live.de>
2020-06-15 18:01:37 +0200

    Add basic output fluid handler slot

---
+++ src/main/java/info/u_team/u_mod/container/basic/BasicOutputFluidHandlerSlot.java
@@ -0,0 +1,18 @@
+package info.u_team.u_mod.container.basic;
+
+import info.u_team.u_team_core.api.fluid.IFluidHandlerModifiable;
+import info.u_team.u_team_core.container.FluidSlot;
+import net.minecraftforge.fluids.FluidStack;
+
+public class BasicOutputFluidHandlerSlot extends FluidSlot {
+	
+	public BasicOutputFluidHandlerSlot(IFluidHandlerModifiable fluidHandler, int index, int x, int y) {
+		super(fluidHandler, index, x, y);
+	}
+	
+	@Override
+	public boolean isFluidValid(FluidStack stack) {
+		return false;
+	}
+	
+}
