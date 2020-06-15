9a0ab5e9d01aa5c74a6f98843fb4cc1815bd915e
HyCraftHD <hycrafthd@live.de>
2020-06-15 16:53:02 +0200

    Fix basic machine screen

--- src/main/java/info/u_team/u_mod/screen/basic/BasicMachineScreen.java
+++ src/main/java/info/u_team/u_mod/screen/basic/BasicMachineScreen.java
@@ -1,12 +1,12 @@
 package info.u_team.u_mod.screen.basic;
 
-import info.u_team.to_uteam_core.FluidContainerScreen;
 import info.u_team.u_mod.container.basic.BasicMachineContainer;
+import info.u_team.u_team_core.gui.UContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 
-public class BasicMachineScreen<T extends BasicMachineContainer<?>> extends FluidContainerScreen<T> {
+public class BasicMachineScreen<T extends BasicMachineContainer<?>> extends UContainerScreen<T> {
 	
 	public BasicMachineScreen(T container, PlayerInventory playerInventory, ITextComponent title, ResourceLocation background) {
 		super(container, playerInventory, title, background);
