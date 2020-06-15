268dcd66f642e075c0a25e1f1e0791ec11b65ff3
HyCraftHD <hycrafthd@live.de>
2020-06-15 18:00:25 +0200

    The slots now extends the new item slot class

--- src/main/java/info/u_team/u_mod/container/basic/BasicOutputItemHandlerSlot.java
+++ src/main/java/info/u_team/u_mod/container/basic/BasicOutputItemHandlerSlot.java
@@ -1,9 +1,10 @@
 package info.u_team.u_mod.container.basic;
 
+import info.u_team.u_team_core.container.ItemSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.*;
 
-public class BasicOutputItemHandlerSlot extends SlotItemHandler {
+public class BasicOutputItemHandlerSlot extends ItemSlot {
 	
 	public BasicOutputItemHandlerSlot(IItemHandler itemHandler, int index, int xPosition, int yPosition) {
 		super(itemHandler, index, xPosition, yPosition);
--- src/main/java/info/u_team/u_mod/container/basic/BasicUpgradeItemHandlerSlot.java
+++ src/main/java/info/u_team/u_mod/container/basic/BasicUpgradeItemHandlerSlot.java
@@ -1,10 +1,11 @@
 package info.u_team.u_mod.container.basic;
 
 import info.u_team.u_mod.item.basic.BasicMachineUpgradeItem;
+import info.u_team.u_team_core.container.ItemSlot;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.items.*;
+import net.minecraftforge.items.IItemHandler;
 
-public class BasicUpgradeItemHandlerSlot extends SlotItemHandler {
+public class BasicUpgradeItemHandlerSlot extends ItemSlot {
 	
 	public BasicUpgradeItemHandlerSlot(IItemHandler itemHandler, int index, int xPosition, int yPosition) {
 		super(itemHandler, index, xPosition, yPosition);
