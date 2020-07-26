c46cd6fdac0381dfad2fc0e38022b0068a057c7c
HyCraftHD <hycrafthd@live.de>
2020-07-26 22:55:12 +0200

    Update container types to deferred register

--- src/main/java/info/u_team/u_mod/init/UModContainerTypes.java
+++ src/main/java/info/u_team/u_mod/init/UModContainerTypes.java
@@ -6,15 +6,16 @@ import info.u_team.u_team_core.containertype.UContainerType;
 import info.u_team.u_team_core.util.registry.CommonDeferredRegister;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class UModContainerTypes {
 	
 	public static final CommonDeferredRegister<ContainerType<?>> CONTAINER_TYPES = CommonDeferredRegister.create(ForgeRegistries.CONTAINERS, UMod.MODID);
 	
-	public static final UContainerType<ElectricFurnaceContainer> ELECTRIC_FURNACE = new UContainerType<>("electric_furnace", ElectricFurnaceContainer::new);
-	public static final UContainerType<CrusherContainer> CRUSHER = new UContainerType<>("crusher", CrusherContainer::new);
-	public static final UContainerType<OreWasherContainer> ORE_WASHER = new UContainerType<>("ore_washer", OreWasherContainer::new);
+	public static final RegistryObject<UContainerType<ElectricFurnaceContainer>> ELECTRIC_FURNACE = CONTAINER_TYPES.register("electric_furnace", () -> new UContainerType<>(ElectricFurnaceContainer::new));
+	public static final RegistryObject<UContainerType<CrusherContainer>> CRUSHER = CONTAINER_TYPES.register("crusher", () -> new UContainerType<>(CrusherContainer::new));
+	public static final RegistryObject<UContainerType<OreWasherContainer>> ORE_WASHER = CONTAINER_TYPES.register("ore_washer", () -> new UContainerType<>(OreWasherContainer::new));
 	
 	public static void register(IEventBus bus) {
 		CONTAINER_TYPES.register(bus);
