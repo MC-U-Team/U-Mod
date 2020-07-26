c70724d6a4ca8be18253e942f7e1f3987a19cac8
HyCraftHD <hycrafthd@live.de>
2020-07-26 22:46:05 +0200

    Register the blocks wit the deferred register now

--- src/main/java/info/u_team/u_mod/init/UModBlocks.java
+++ src/main/java/info/u_team/u_mod/init/UModBlocks.java
@@ -3,20 +3,21 @@ package info.u_team.u_mod.init;
 import info.u_team.u_mod.UMod;
 import info.u_team.u_mod.block.*;
 import info.u_team.u_mod.block.basic.BasicMachineBlock;
-import info.u_team.u_team_core.util.registry.BlockDeferredRegister;
+import info.u_team.u_team_core.util.registry.*;
+import net.minecraft.item.BlockItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 
 public class UModBlocks {
 	
 	public static final BlockDeferredRegister BLOCKS = BlockDeferredRegister.create(UMod.MODID);
 	
-	public static final CableBlock CABLE = new CableBlock("cable_test");
+	public static final BlockRegistryObject<CableBlock, BlockItem> CABLE = BLOCKS.register("cable_test", () -> new CableBlock("cable_test"));
 	
-	public static final EnergyStorageBlock ENERGY_STORAGE = new EnergyStorageBlock("energy_storage");
+	public static final BlockRegistryObject<EnergyStorageBlock, BlockItem> ENERGY_STORAGE = BLOCKS.register("energy_storage", () -> new EnergyStorageBlock("energy_storage"));
 	
-	public static final BasicMachineBlock ELECTRIC_FURNACE = new BasicMachineBlock("electric_furnace", () -> UModTileEntityTypes.ELECTRIC_FURNACE);
-	public static final BasicMachineBlock CRUSHER = new BasicMachineBlock("crusher", () -> UModTileEntityTypes.CRUSHER);
-	public static final BasicMachineBlock ORE_WASHER = new BasicMachineBlock("ore_washer", () -> UModTileEntityTypes.ORE_WASHER);
+	public static final BlockRegistryObject<BasicMachineBlock, BlockItem> ELECTRIC_FURNACE = BLOCKS.register("electric_furnace", () -> new BasicMachineBlock("electric_furnace", () -> UModTileEntityTypes.ELECTRIC_FURNACE));
+	public static final BlockRegistryObject<BasicMachineBlock, BlockItem> CRUSHER = BLOCKS.register("crusher", () -> new BasicMachineBlock("crusher", () -> UModTileEntityTypes.CRUSHER));
+	public static final BlockRegistryObject<BasicMachineBlock, BlockItem> ORE_WASHER = BLOCKS.register("ore_washer", () -> new BasicMachineBlock("ore_washer", () -> UModTileEntityTypes.ORE_WASHER));
 	
 	public static void register(IEventBus bus) {
 		BLOCKS.register(bus);
